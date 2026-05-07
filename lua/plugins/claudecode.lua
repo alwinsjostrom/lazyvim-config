9004a300b1d52f18201caf3b526cfaf64b071d19
alwinsjostrom <alwinsjostrom@yahoo.com>
2026-05-07 10:43:51 +0200

    Claude should stay in termial when showing diffs

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,11 @@
+return {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    opts = {
+        diff_opts = {
+            keep_terminal_focus = true,
+        },
+    },
+    keys = {
+    },
+}
