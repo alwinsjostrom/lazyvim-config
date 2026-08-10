42abe405c9d860ed419068c73c5e540fe7579a2e
alwinsjostrom <alwinsjostrom@yahoo.com>
2026-08-10 09:50:03 +0200

    Customize dashboard graphics and use transparent backgrounds

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,18 @@
+return {
+    "folke/snacks.nvim",
+    opts = {
+        dashboard = {
+            preset = {
+                header = [[
+      .o.       oooo  oooooo     oooo  o8o                    
+     .888.      `888   `888.     .8'   `"'                    
+    .8"888.      888    `888.   .8'   oooo  ooo. .oo.  .oo.   
+   .8' `888.     888     `888. .8'    `888  `888P"Y88bP"Y88b  
+  .88ooo8888.    888      `888.8'      888   888   888   888  
+ .8'     `888.   888       `888'       888   888   888   888  
+o88o     o8888o o888o       `8'       o888o o888o o888o o888o 
+                ]],
+            },
+        },
+    },
+}
